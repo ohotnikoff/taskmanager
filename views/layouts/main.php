656c7214cff334942a966a45920fbb9ea8851ade
Dmitry <od@od7.ru>
2020-12-15 06:08:21 +0500

    установка шрифта, вывод шапки

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?= $this->title ?></title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
 </head>
 <body>
+    <header class="py-3">
+        <div class="container">
+            <div class="text-center">
+                <a class="header-logo text-dark" href="#">TaskManager</a>
+            </div>
+
+            <nav class="nav d-flex justify-content-center">
+                <a class="p-2 text-muted active" href="#">Главная</a>
+                <a class="p-2 text-muted" href="#">Сотрудники</a>
+                <a class="p-2 text-muted" href="#">Задачи</a>
+            </nav>
+        </div>
+    </header>
 
     <?= $content ?>
 
